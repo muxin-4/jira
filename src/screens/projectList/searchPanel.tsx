af854cd566ea728b2f7efca44097a943e0703fa4
muxin-4 <wgy952046097@gmail.com>
2022-01-29 16:09:54 +0800

    chore: 添加ts类型定义

---
+++ src/screens/projectList/searchPanel.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+export const SearchPanel = (props: SearchPanelProps) => {
+  const { users, param, setParam } = props;
+
+  return (
+    <form action="">
+      {/* setParam(Object.assign({ }, param, {name: evt.target.value})) */}
+      <input
+        type="text"
+        value={param.name}
+        onChange={(evt) =>
+          setParam({
+            ...param,
+            name: evt.target.value,
+          })
+        }
+      />
+      <select
+        value={param.personId}
+        onChange={(evt) =>
+          setParam({
+            ...param,
+            personId: evt.target.value,
+          })
+        }
+      >
+        <option value={""}>负责人</option>
+        {users.map((item) => {
+          return (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
+};
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   console.log("aaa1", object);
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
@@ -17,15 +19,16 @@ export const cleanObject = (object) => {
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const debounce = (func, delay) => {
+export const debounce = (func: () => void, delay: number) => {
   console.log("debounce1");
-  let timeout;
+  let timeout: ReturnType<typeof setTimeout>;
+
   return () => {
     console.log("debounce2");
     if (timeout) {
@@ -40,7 +43,7 @@ export const debounce = (func, delay) => {
   };
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
