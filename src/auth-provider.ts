557a3043be0da83ede5cbbec32239503e00978c6
muxin-4 <wgy952046097@gmail.com>
2022-01-30 17:54:26 +0800

    feat: jwt

---
+++ src/auth-provider.ts
@@ -0,0 +1,42 @@
+// 在真实环境中，如果使用firebase这种第三方服务的话，本文件不需要开发
+
+import { User } from "screens/projectList/searchPanel";
+
+const localStorageKey = "__auth_provider_token__";
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const login = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response: Response) => {
+    if (response.ok) {
+    }
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response: Response) => {
+    if (response.ok) {
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorageKey);
