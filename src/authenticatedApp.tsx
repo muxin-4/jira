9f1dfcb07b4209db491278b8ca8145a6a864e4e1
muxin-4 <wgy952046097@gmail.com>
2022-02-01 15:15:28 +0800

    feat: 使用useAuth切换登录注册2

--- src/authenticatedApp.tsx
+++ src/authenticatedApp.tsx
@@ -6,6 +6,7 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
+      <button onClick={logout}></button>
       <ProjectListScreen />
     </div>
   );
